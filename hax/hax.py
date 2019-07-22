a132608a265b17917c7d5322e5b89b3dbb6dd280
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2019-07-22 08:40:52 +0000

    Implemented publisher thread

--- hax/hax.py
+++ hax/hax.py
@@ -1,37 +1,47 @@
 from hax.halink import HaLink
 from hax.server import run_server
 from hax.types import Fid
+from queue import Queue
+from threading import Thread
+import consul
 import logging
-import threading
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] {%(threadName)s} %(message)s')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s [%(levelname)s] {%(threadName)s} %(message)s')
 
 
 def thread_fn(ha: HaLink):
     ha.test()
 
 
+def kv_publisher_thread(q: Queue):
+    logging.info('Publisher thread has started')
+    try:
+        client = consul.Consul()
+        while True:
+            item = q.get()
+            logging.debug('Sending message to Consul: {}'.format(item))
+            # TODO what and where do we actually need to persist to KV?
+            # client.kv.put('bq', item)
+    finally:
+        logging.info('Publisher thread has exited')
+
+
+def run_publisher_thread(q: Queue):
+    t = Thread(target=kv_publisher_thread, args=(q, ))
+    t.start()
+
 def main():
     setup_logging()
-    run_server()
+    q = Queue(maxsize=1000)
+    run_publisher_thread(q)
+    run_server(q)
 
     l = HaLink(node_uuid="This is a test")
     # l.start("endpoint", Fid(3,4), Fid(5,6), Fid(0xDEADBEEF, 7))
-
-    logging.info("Invoking threads")
-
-    threads = [None] * 20
-    threads = list(map(lambda t: threading.Thread(target=thread_fn, args=(l,)), threads))
-
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    logging.info("Threads finished")
 
 
 if __name__ == "__main__":
--- hax/hax/server.py
+++ hax/hax/server.py
@@ -4,6 +4,10 @@ import json
 
 
 class KVHandler(BaseHTTPRequestHandler):
+    def __init__(self, req, client_addr, server):
+        super().__init__(req, client_addr, server)
+        self.server = server
+
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -18,16 +22,23 @@ class KVHandler(BaseHTTPRequestHandler):
         self._set_headers()
 
     def do_POST(self):
+        queue = self.server.reply_queue
         self._set_headers()
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         logging.info("A new request has been received: {}".format(post_data))
+        queue.put(post_data)
+        logging.debug(
+            "The message is put to the queue, the server thread is free again")
 
 
-def run_server(server_class=HTTPServer, port=8080):
+
+def run_server(queue, server_class=HTTPServer, port=8080):
     port = 8080
     server_address = ('', port)
     httpd = server_class(server_address, KVHandler)
+    httpd.reply_queue = queue
 
     logging.info('Starting http server...')
     httpd.serve_forever()
+    logging.info('The http server has stopped')
