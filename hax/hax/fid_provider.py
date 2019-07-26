cf8a230431c336e015aa24f3f0e3fb9d6371fcc0
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2019-07-26 15:10:40 +0000

    hax takes its endpoint from Consul

--- hax/hax/fid_provider.py
+++ hax/hax/fid_provider.py
@@ -10,11 +10,25 @@ class FidProvider(object):
         self.cns = c.Consul()
 
     def get_hax_fid(self):
+        serv = self.get_local_service_by_name('hax')
+        return Fid.parse(serv.get('ID'))
+
+    def get_local_service_by_name(self, name):
         services = self.cns.agent.services()
         for k, v in services.items():
-            if v.get('Service') == 'hax':
-                return Fid.parse(k)
-        raise RuntimeError('No hax service found in Consul')
+            if v.get('Service') == name:
+                return v
+        raise RuntimeError('No {} service found in Consul'.format(name))
+
+    def get_hax_endpoint(self):
+        my_fid = self.get_hax_fid()
+        _, services = self.cns.catalog.service(service='hax')
+        data = list(
+            map(
+                self._to_canonical_service_data,
+                filter(lambda x: Fid.parse(x.get('ServiceID')) == my_fid,
+                       services)))
+        return data[0].get('address')
 
     def get_leader_session(self):
         _, leader = self.cns.kv.get('leader')
@@ -25,25 +39,21 @@ class FidProvider(object):
         principal_rm = sess_details.get('Node')
         return principal_rm
 
-    def _service_to_confd(self, service):
+    def _to_canonical_service_data(self, service):
         node = service.get('Node')
         fid = service.get('ServiceID')
         address = service.get('Address')
         srv_address = service.get('ServiceAddress')
         srv_port = service.get('ServicePort')
         return {
-            'node':
-            node,
-            'fid':
-            Fid.parse(fid),
-            'address':
-            '{}{}:{}'.format(address, srv_address, srv_port)
+            'node': node,
+            'fid': Fid.parse(fid),
+            'address': '{}{}:{}'.format(address, srv_address, srv_port)
         }
 
     def get_confd_list(self):
         _, services = self.cns.catalog.service(service='confd')
 
         confd_list = list(
-            map(self._service_to_confd,
-                filter(lambda x: x.get('ServiceName') == 'confd', services)))
+            map(self._to_canonical_service_data, services))
         return confd_list
