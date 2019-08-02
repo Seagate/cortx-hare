728604854d62413093857c29cd3c897b892826df
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2019-08-02 11:19:47 +0000

    Introduce m0_ha_note struct at Python level

--- hax/hax/types.py
+++ hax/hax/types.py
@@ -10,6 +10,47 @@ class Uint128Struct(c.Structure):
     _fields_ = [("hi", c.c_uint64), ("lo", c.c_uint64)]
 
 
+class HaNoteStruct(c.Structure):
+    # Constants for no_state field values as they are described in ha/note.h
+
+    # /** Object state is unknown. */
+    M0_NC_UNKNOWN = 0
+    # /** Object can be used normally. */
+    M0_NC_ONLINE = 1
+    # /**
+    # * Object has experienced a permanent failure and cannot be
+    # * recovered.
+    # */
+    M0_NC_FAILED = 2
+    # /**
+    # * Object is experiencing a temporary failure. Halon will notify Mero
+    # * when the object is available for use again.
+    # */
+    M0_NC_TRANSIENT = 3
+    # /**
+    # * This state is only applicable to the pool objects. In this state,
+    # * the pool is undergoing repair, i.e., the process of reconstructing
+    # * data lost due to a failure and storing them in spare space.
+    # */
+    M0_NC_REPAIR = 4
+    # /**
+    # * This state is only applicable to the pool objects. In this state,
+    # * the pool device has completed sns repair. Its data is re-constructed
+    # * on its corresponding spare space.
+    # */
+    M0_NC_REPAIRED = 5
+    # /**
+    # * This state is only applicable to the pool objects. Rebalance process
+    # * is complementary to repair: previously reconstructed data is being
+    # * copied from spare space to the replacement storage.
+    # */
+    M0_NC_REBALANCE = 6
+
+    M0_NC_NR = 7
+
+    _fields_ = [("no_id", FidStruct), ("no_state", c.c_uint32)]
+
+
 # Duplicates mero/fid/fid.h
 class Fid(object):
     def __init__(self, container, key):
