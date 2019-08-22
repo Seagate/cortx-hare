6a511d810e0f7069e716555ebc2d5324cfd5c912
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2019-08-22 03:39:25 +0100

    install.sh: helper added

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+# Helper script to install the stuff on the local node
+
+export SRC_DIR="$(dirname $(readlink -f $0))"
+
+sudo mkdir -p /opt/seagate
+sudo ln -sfn $SRC_DIR /opt/seagate/consul
+
+sudo cp systemd/* /usr/lib/systemd/system/
+sudo systemctl daemon-reload
