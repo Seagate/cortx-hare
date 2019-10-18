dd863e8092b5f66c18ff208eeb2235e55d3bbcdf
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2019-10-18 11:06:14 +0000

    Problem: hax doesn't use type hints

--- hax/hax/util.py
+++ hax/hax/util.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any, Dict, NamedTuple
 
 from consul import Consul
 
@@ -7,9 +8,11 @@ from hax.types import Fid
 
 SERVICE_CONTAINER = 0x7300000000000001
 PROCESS_CONTAINER = 0x7200000000000001
+ServiceData = NamedTuple('ServiceData', [('node', str), ('fid', Fid),
+                                         ('address', str)])
 
 
-def create_process_fid(key):
+def create_process_fid(key: Any) -> Fid:
     """
     Returns a correct Fid instance by the given fidk value. The resulting
     Fid will correspond to a Mero process.
@@ -17,13 +20,13 @@ def create_process_fid(key):
     return Fid(PROCESS_CONTAINER, int(key))
 
 
-def _to_service_fid(key: int):
+def _to_service_fid(key: int) -> Fid:
     return Fid(SERVICE_CONTAINER, int(key))
 
 
 class ConsulUtil:
     def __init__(self):
-        self.cns = Consul()
+        self.cns: Consul = Consul()
         self.event_map = {
             0: "M0_CONF_HA_PROCESS_STARTING",
             1: "M0_CONF_HA_PROCESS_STARTED",
@@ -31,7 +34,7 @@ class ConsulUtil:
             3: "M0_CONF_HA_PROCESS_STOPPED"
         }
 
-    def get_hax_fid(self):
+    def get_hax_fid(self) -> Fid:
         """
         Returns the fid of the current hax process (in other words, returns
         "my own" fid)
@@ -39,12 +42,12 @@ class ConsulUtil:
         serv = self.get_local_service_by_name('hax')
         return create_process_fid(serv['ServiceID'])
 
-    def get_ha_fid(self):
+    def get_ha_fid(self) -> Fid:
         serv = self.get_local_service_by_name('hax')
         fidk = int(serv['ServiceID'])
         return _to_service_fid(fidk + 1)
 
-    def get_rm_fid(self):
+    def get_rm_fid(self) -> Fid:
         lsess = self.get_leader_session()
         p_rm_node = self.get_session_node(lsess)
         serv = self.get_node_service_by_name(p_rm_node, 'confd')
@@ -53,17 +56,17 @@ class ConsulUtil:
         sfidk = self.cns.kv.get(key)[1]
         return _to_service_fid(int(sfidk['Value']))
 
-    def get_my_nodename(self):
-        return self.cns.agent.self()['Config']['NodeName']
+    def get_my_nodename(self) -> str:
+        return str(self.cns.agent.self()['Config']['NodeName'])
 
-    def get_node_service_by_name(self, hostname, svc_name):
+    def get_node_service_by_name(self, hostname, svc_name) -> Dict[str, Any]:
         for svc in self.cns.catalog.service(service=svc_name)[1]:
             if svc['Node'] == hostname:
                 return svc
         raise HAConsistencyException(
             f'No {svc_name!r} Consul service found at node {hostname!r}')
 
-    def get_local_service_by_name(self, name):
+    def get_local_service_by_name(self, name: str) -> Dict[str, Any]:
         """
         Returns the service data by its name assuming that it runs at the same
         node to the current hax process.
@@ -71,37 +74,35 @@ class ConsulUtil:
         hostname = self.get_my_nodename()
         return self.get_node_service_by_name(hostname, name)
 
-    def get_hax_endpoint(self):
+    def get_hax_endpoint(self) -> str:
         my_fid = self.get_hax_fid()
         services = self.cns.catalog.service(service='hax')[1]
         data = list(
             map(self._to_canonical_service_data,
                 filter(lambda x: int(x['ServiceID']) == my_fid.key, services)))
-        return data[0]['address']
+        return data[0].address
 
-    def get_leader_session(self):
+    def get_leader_session(self) -> str:
         leader = self.cns.kv.get('leader')[1]
         session = leader.get('Session')
         if not session:
             raise HAConsistencyException(
                 'Could not get the leader from Consul')
-        return session
+        return str(session)
 
-    def get_session_node(self, session_id):
+    def get_session_node(self, session_id: str) -> str:
         sess_details = self.cns.session.info(session_id)[1]
-        return sess_details.get('Node')  # principal RM
+        return str(sess_details.get('Node'))  # principal RM
 
     @staticmethod
-    def _to_canonical_service_data(service):
+    def _to_canonical_service_data(service: Dict[str, Any]) -> ServiceData:
         node = service['Node']
         fidk = service['ServiceID']
         srv_address = service['ServiceAddress']
         srv_port = service['ServicePort']
-        return {
-            'node': node,
-            'fid': create_process_fid(fidk),
-            'address': f'{srv_address}:{srv_port}'
-        }
+        return ServiceData(node=node,
+                           fid=create_process_fid(fidk),
+                           address=f'{srv_address}:{srv_port}')
 
     def get_confd_list(self):
         services = self.cns.catalog.service(service='confd')[1]
