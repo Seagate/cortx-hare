2db8a70c543b0bfe366d9c5037eeb1d4beb0104b
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2019-12-28 22:32:52 +0200

    Problem: Pacemaker HA solution is not tested by CI
    Solution: add `test-pcs` CI job.
    
    Note, that current implementation of `ci/test-pcs` script only covers
    sections 1–2 of the [EES HA HOWTO][].  The script should also invoke
    Andriy's `utils/build-ees-ha`, which covers section 3 of the HOWTO.
    This will be done in another patch.
    
    Related issue: #197
    
    [EES HA HOWTO]: https://docs.google.com/document/d/1zhlBMF1usZtMmLIDwl7s8h9QgwGkHy2B4wMhetZPrN4/edit

--- ci/functions.sh
+++ ci/functions.sh
@@ -11,43 +11,76 @@ _time() {
     fi
 }
 
-# CAUTION: ci_job_label cannot be used in "m0vg" environment, because
-# WORKSPACE_NAME and CI_JOB_NAME are not defined there.
-ci_job_label() {
-    echo "${WORKSPACE_NAME}-${CI_JOB_NAME}"
+ci_vm_name_prefix() {
+    local job_name=
+    case $# in
+        0)
+            job_name=$CI_JOB_NAME
+            ;;
+        1)  # "m0vg" environment
+            [[ -z ${CI_JOB_NAME:-} ]] ||
+                die "${FUNCNAME[0]}: CI_JOB_NAME is not expected to be set"
+            job_name=$1
+            ;;
+        *)
+            die "${FUNCNAME[0]}: Too many arguments"
+            ;;
+    esac
+    echo "${WORKSPACE_NAME}-$job_name"
 }
 
 ci_init_m0vg() (
-    case $# in
-        1) local m0vg_dir=m0vg-1node;;
-        2) local m0vg_dir=m0vg-2nodes;;
-        *) die "Usage: ${FUNCNAME[0]} HOST [HOST_2]";;
+    [[ $M0VG == m0vg-$CI_JOB_NAME/scripts/m0vg ]] ||
+        die "${FUNCNAME[0]}: Invalid M0VG"
+
+    local hosts=
+    local opt_provision='--no-provision'
+
+    case $CI_JOB_NAME in
+        test-boot1)
+            hosts=(cmu)
+            ;;
+        test-boot2)
+            hosts=(ssu1 ssu2)
+            ;;
+        test-pcs)
+            hosts=(cmu pod-c1 pod-c2)
+            opt_provision=
+            ;;
+        *) "${FUNCNAME[0]}: Invalid CI_JOB_NAME";;
     esac
-    [[ $M0VG == $m0vg_dir/scripts/m0vg ]] ||
-        die "${FUNCNAME[0]}: Impossible happened"
 
     cd $WORKSPACE_DIR
 
-    if [[ ! -d $m0vg_dir ]]; then
-        # Get `m0vg` script ($m0vg_dir/scripts/m0vg).
+    if [[ ! -x $M0VG ]]; then
+        # Get `m0vg` script.
         # Note that we download the latest Mero, disregarding
         # `MERO_COMMIT_REF`.
         git clone --recursive --depth 1 --shallow-submodules \
-            http://gitlab.mero.colo.seagate.com/mero/mero.git $m0vg_dir
+            http://gitlab.mero.colo.seagate.com/mero/mero.git \
+            ${M0VG%/scripts/m0vg}
     fi
 
     $M0VG env add <<EOF
 M0_VM_BOX=centos76/dev
 M0_VM_BOX_URL='http://ci-storage.mero.colo.seagate.com/vagrant/centos76/dev'
 M0_VM_CMU_MEM_MB=4096
-M0_VM_NAME_PREFIX=$(ci_job_label)
-M0_VM_HOSTNAME_PREFIX=$(ci_job_label)
+M0_VM_NAME_PREFIX=$(ci_vm_name_prefix)
+M0_VM_HOSTNAME_PREFIX=$(ci_vm_name_prefix)
 EOF
+    if [[ $CI_JOB_NAME == 'test-pcs' ]]; then
+        $M0VG env add M0_VM_POD_SIMULATION=yes M0_VM_POD_DISKS=10
+    fi
 
     local host=
-    for host in "$@"; do
-        _time $M0VG up --no-provision $host
-        _time $M0VG reload --no-provision $host
+    for host in ${hosts[@]}; do
+        _time $M0VG up $opt_provision $host
+        _time $M0VG reload $opt_provision $host
+    done
+
+    # 'hostmanager' plugin should be executed last, when all machines are up.
+    for host in ${hosts[@]}; do
+        $M0VG provision --provision-with hostmanager $host
     done
 )
 
