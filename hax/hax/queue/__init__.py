94fe98991ee245c131eb2f00b2a8e42a1fc72df2
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2020-09-11 08:44:56 -0600

    Problem: STOB error are not broadcast
    Solution:
    1. Add EQ rule that expects ioq_stob message type (with StobIoqError
    dumped as JSON)
    2. That rule forwards the StobIoqError to BQ with message type IOQ_STOB_ERROR
    3. Add BQ handler for IOQ_STOB_ERROR that takes conf_sdev Fid and sends
    HAState with it as 'offline' (M0_NC_FAILED).

--- hax/hax/queue/__init__.py
+++ hax/hax/queue/__init__.py
@@ -1,12 +1,12 @@
 import json
 import logging
 from queue import Queue
-from typing import Any, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from hax.message import BroadcastHAStates
 from hax.motr.delivery import DeliveryHerald
 from hax.queue.confobjutil import ConfObjUtil
-from hax.types import HaLinkMessagePromise, HAState, MessageId
+from hax.types import Fid, HaLinkMessagePromise, HAState, MessageId
 from hax.util import create_drive_fid
 
 
@@ -24,28 +24,59 @@ class BQProcessor:
         (i, msg) = message
         logging.debug('Message #%d received: %s (type: %s)', i, msg,
                       type(msg).__name__)
-        self.payload_process(msg)
+        try:
+            self.payload_process(msg)
+        except Exception:
+            logging.exception(
+                'Failed to process a message #%d.'
+                ' The message is skipped.', i)
         logging.debug('Message #%d processed', i)
 
     def payload_process(self, msg: str) -> None:
-        hastates = []
+        data = None
         try:
-            msg_load = json.loads(msg)
-            payload = msg_load['payload']
+            data = json.loads(msg)
         except json.JSONDecodeError:
             logging.error('Cannot parse payload, invalid json')
             return
+
+        payload = data['payload']
+        msg_type = data['message_type']
+
+        handlers: Dict[str, Callable[[Dict[str, Any]], None]] = {
+            'M0_HA_MSG_NVEC': self.handle_device_state_set,
+            'STOB_IOQ_ERROR': self.handle_ioq_stob_error,
+        }
+        if msg_type not in handlers:
+            logging.warn(
+                'Unsupported message type given: %s. Message skipped.',
+                msg_type)
+            return
+        handlers[msg_type](payload)
+
+    def handle_device_state_set(self, payload: Dict[str, Any]) -> None:
         # To add check for multiple object entries in a payload.
         # for objinfo in payload:
         hastate: Optional[HAState] = self.to_ha_state(payload)
-        if hastate:
-            hastates.append(hastate)
-        if not hastates:
-            logging.debug('No ha states to broadcast')
+        if not hastate:
+            logging.debug('No ha states to broadcast.')
             return
 
         q: Queue = Queue(1)
-        self.queue.put(BroadcastHAStates(states=hastates, reply_to=q))
+        self.queue.put(BroadcastHAStates(states=[hastate], reply_to=q))
+        ids: List[MessageId] = q.get()
+        self.herald.wait_for_any(HaLinkMessagePromise(ids))
+
+    def handle_ioq_stob_error(self, payload: Dict[str, Any]) -> None:
+        fid = Fid.parse(payload['conf_sdev'])
+        if fid.is_null():
+            logging.debug('Fid is 0:0. Skipping the message.')
+            return
+
+        q: Queue = Queue(1)
+        self.queue.put(
+            BroadcastHAStates(states=[HAState(fid, status='offline')],
+                              reply_to=q))
         ids: List[MessageId] = q.get()
         self.herald.wait_for_any(HaLinkMessagePromise(ids))
 
