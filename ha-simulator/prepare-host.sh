1a8a9850b72a862729c429f49cb7366232dfe9bd
Konstantin Nekrasov <konstantin.nekrasov@seagate.com>
2021-08-05 06:37:41 -0600

    EOS-15788: dev env for cortx-ha integration missing
    Solution:
    1. Prepare the Dockerfile where cortx-ha sources get installed so that a
    custom script can use CORTX HA mechanisms can be leveraged for
    simulation.
    
    Signed-off-by: Konstantin Nekrasov <konstantin.nekrasov@seagate.com>

---
+++ ha-simulator/prepare-host.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script performs all the necessary actions to get Hare working against simulated HA and dockerized Kafka.
+
+set -e -x
+
+if [[ ! -f /etc/cortx/ha/ha.conf ]]; then
+  sudo mkdir -p /etc/cortx/ha/
+  sudo cp -fv docker/etc/ha.conf /etc/cortx/ha/ha.conf
+fi
+
+
+if [[ ! -f /etc/cortx/ha/ha.conf ]]; then
+  sudo mkdir -p /etc/cortx/ha/
+  sudo cp -f docker/etc/ha.conf /etc/cortx/ha/ha.conf
+fi
+
+#if [[ -f /etc/cortx/message_bus.conf ]]; then
+  #echo '/etc/cortx/message_bus.conf already exists. Please rename or remove it' >&2
+  #exit 1
+#fi
+
+cat <<EOF | sudo tee /etc/cortx/message_bus.conf > /dev/null
+{
+  "message_broker": {
+    "type": "kafka",
+    "message_bus": {
+      "recv_message_timeout": "100000",
+      "controller_socket_timeout": "100000",
+      "send_message_timeout": "100000"
+    },
+    "cluster": [
+      {
+        "server": "ssc-vm-g2-rhev4-0175.colo.seagate.com",
+        "port": "9093"
+      }
+    ]
+  }
+}
+EOF
