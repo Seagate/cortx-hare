Seagate/cortx-hare

commit 28869a4fb56ce4ceb2b552ff44cb6cd9144d64d4

EOS-27018: Hare to adopt utils logging framework
Problem: MessageBUs class is singleton class and component need to
call MessageBus.init method before making use of Message Bus library

Solution: Calling MessageBus.init method in 'start' interface before
starting other Hare related services

Signed-off-by: Swapnil Gaonkar <swapnil.gaonkar@seagate.com>

hax/hax/hax.py
@@ -38,6 +38,8 @@ from hax.motr.rconfc import RconfcStarter
 from hax.server import ServerRunner
 from hax.types import Fid, Profile, StoppableThread
 from hax.util import ConsulUtil, repeat_if_fails
+from cortx.utils.conf_store import Conf
+from cortx.utils.message_bus import MessageBus
 
 __all__ = ['main']
 
@@ -115,6 +117,21 @@ def _run_rconfc_starter_thread(motr: Motr,
     return rconfc_starter
 
 
+def _ha_message_bus_init(util: ConsulUtil):
+    configpath = util.get_configpath()
+    Conf.load('cortx_conf', configpath)
+
+    message_server_endpoints = Conf.get('cortx_conf',
+                                        'cortx>external>kafka>endpoints')
+
+    """
+    This creates a MessageBus internal global in-memory object which is
+    indirectly part of the process address space. Thus
+    `MessageBus.init()` must be invoked within process's address space.
+    """
+    MessageBus.init(message_server_endpoints)
+
+
 def main():
     # Note: no logging must happen before this call.
     # Otherwise the log configuration will not apply.
@@ -154,6 +171,8 @@ def main():
     LOG.info(f'Setting up ha_link interface with the options as follows: '
              f'hax fid = {cfg.hax_fid}, hax endpoint = {cfg.hax_ep}, '
              f'HA fid = {cfg.ha_fid}')
+
+    _ha_message_bus_init(util)
 
     ffi = HaxFFI()
     herald = DeliveryHerald()

hax/hax/ha/events.py
@@ -20,7 +20,7 @@ import logging
 from dataclasses import dataclass
 from typing import List, Optional
 
-from cortx.utils.message_bus import MessageBus, MessageConsumer
+from cortx.utils.message_bus import MessageConsumer
 
 from ha.core.event_manager.subscribe_event import SubscribeEvent
 from ha.core.event_manager.const import EVENT_MANAGER_KEYS
@@ -75,9 +75,7 @@ class EventListener():
         if topic is None:
             raise RuntimeError('Failed to subscribe to events')
 
-        message_bus = MessageBus()
         self.consumer = MessageConsumer(
-            message_bus=message_bus,
             consumer_id=COMPONENT_ID,
             consumer_group=group_id,
             message_types=[topic],

hax/hax/util.py
@@ -1825,6 +1825,15 @@ class ConsulUtil:
         if not self.kv.kv_delete_in_transaction(keys):
             raise HAConsistencyException('KV deletion failed')
 
+    @repeat_if_fails()
+    def get_configpath(self):
+        logging.info('Getting config_path')
+        config_path = self.kv.kv_get('config_path')
+        if config_path is None:
+            raise HAConsistencyException('config path not present in consul')
+
+        return config_path['Value'].decode("utf-8")
+
 
 def dump_json(obj) -> str:
     """

provisioning/miniprov/hare_mp/main.py
@@ -309,6 +309,7 @@ def prepare(args):
     log_dir = get_log_dir(url)
     _create_consul_namespace(conf_dir)
     consul_starter = _start_consul(utils, stop_event, conf_dir, log_dir, url)
+    utils.save_config_path(url)
     utils.save_node_facts()
     utils.save_drives_info()
     try:
@@ -373,6 +374,7 @@ def start_hax_and_consul_without_systemd(url: str, utils: Utils):
 
 
 def start(args):
+    logging.info('Starting Hare services')
     url = args.config[0]
     utils = Utils(ConfStoreProvider(url))
     logrotate_generic(url)

provisioning/miniprov/hare_mp/utils.py
@@ -201,6 +201,10 @@ class Utils:
             return 'libfab'
         return transport_type
 
+    @repeat_if_fails()
+    def save_config_path(self, path: str):
+        self.kv.kv_put('config_path', path)
+
 
 class LogWriter:
     def __init__(self, logger: logging.Logger, logging_handler):

commit 1d36361d45ba896bea877ccc9308869fff138876

EOS-27315: hctl bootstrap is failing in latest hare main
hctl bootstrap hangs in main,
```
[root@ssc-vm-g2-rhev4-1947 ~]# hctl bootstrap --mkfs --xprt libfab ~/1n.yaml
2022-01-07 10:44:17: Generating cluster configuration... OK
2022-01-07 10:44:20: Starting Consul server on this node......... OK
2022-01-07 10:44:29: Importing configuration into the KV store... OK
2022-01-07 10:44:31: Starting Consul on other nodes...Consul ready on all nodes
2022-01-07 10:44:31: Updating Consul configuraton from the KV store... OK
2022-01-07 10:44:33: Waiting for the RC Leader to get elected.... OK
2022-01-07 10:44:36: Starting Motr (phase1, mkfs)...
```

Because config_path kv is missing is missing in Consul, config_path is added through hare mini provisioner, which is not invoked as part of hctl bootstrap.
```
Jan 07 10:49:18 ssc-vm-g2-rhev4-1947.colo.seagate.com hare-hax[6027]: 2022-01-07 10:49:18,919 [DEBUG] {MainThread} Got HAConsistencyException: config path not present in consul while invokingJan 07 10:49:23 ssc-vm-g2-rhev4-1947.colo.seagate.com hare-hax[6027]: 2022-01-07 10:49:23,924 [DEBUG] {MainThread} KVGET key=config_path, kwargs={}
```

This is a regression from commit 28869a4f.

Solution:
- Move `MessageBus.init` to ha module, thus the relevant code will be
invoked only if deployed with cortx-ha component.
- Install m0ping utility as part of hare `make devinstall`

Signed-off-by: Mandar Sawant <mandar.sawant@seagate.com>

Makefile
@@ -430,6 +430,7 @@ devinstall-hax: hax/requirements.txt $(HAX_EGG_LINK)
 	@ln -v -sf $(PY_VENV_DIR)/bin/q $(DESTDIR)/$(PREFIX)/bin
 	@ln -v -sf $(PY_VENV_DIR)/bin/configure $(DESTDIR)/$(PREFIX)/bin
 	@ln -v -sf $(PY_VENV_DIR)/bin/update-conf $(DESTDIR)/$(PREFIX)/bin
+	@ln -v -sf $(PY_VENV_DIR)/bin/m0ping $(DESTDIR)/$(PREFIX)/bin
 
 .PHONY: devinstall-miniprov
 devinstall-miniprov: MP_INSTALL_CMD = $(SETUP_PY) develop

hax/hax/ha/__init__.py
@@ -22,6 +22,8 @@ import logging
 from hax.motr.planner import WorkPlanner
 from hax.types import StoppableThread
 from hax.util import ConsulUtil
+from cortx.utils.message_bus import MessageBus
+from cortx.utils.conf_store import Conf
 
 __all__ = ['create_ha_thread']
 
@@ -48,6 +50,21 @@ def create_ha_thread(planner: WorkPlanner,
     """Creates the proper HA-aware thread, handling possible import errors."""
     try:
         from .thread import EventPollingThread
+        _ha_message_bus_init(util)
         return EventPollingThread(planner, util)
     except ImportError:
         return StubEventThread()
+
+
+def _ha_message_bus_init(util: ConsulUtil):
+    """
+    This creates a MessageBus internal global in-memory object which is
+    indirectly part of the process address space. Thus
+    `MessageBus.init()` must be invoked within process's address space.
+    """
+    configpath = util.get_configpath()
+    Conf.load('cortx_conf', configpath)
+
+    message_server_endpoints = Conf.get('cortx_conf',
+                                        'cortx>external>kafka>endpoints')
+    MessageBus.init(message_server_endpoints)

hax/hax/hax.py
@@ -38,8 +38,7 @@ from hax.motr.rconfc import RconfcStarter
 from hax.server import ServerRunner
 from hax.types import Fid, Profile, StoppableThread
 from hax.util import ConsulUtil, repeat_if_fails
-from cortx.utils.conf_store import Conf
-from cortx.utils.message_bus import MessageBus
+
 
 __all__ = ['main']
 
@@ -117,21 +116,6 @@ def _run_rconfc_starter_thread(motr: Motr,
     return rconfc_starter
 
 
-def _ha_message_bus_init(util: ConsulUtil):
-    configpath = util.get_configpath()
-    Conf.load('cortx_conf', configpath)
-
-    message_server_endpoints = Conf.get('cortx_conf',
-                                        'cortx>external>kafka>endpoints')
-
-    """
-    This creates a MessageBus internal global in-memory object which is
-    indirectly part of the process address space. Thus
-    `MessageBus.init()` must be invoked within process's address space.
-    """
-    MessageBus.init(message_server_endpoints)
-
-
 def main():
     # Note: no logging must happen before this call.
     # Otherwise the log configuration will not apply.
@@ -171,8 +155,6 @@ def main():
     LOG.info(f'Setting up ha_link interface with the options as follows: '
              f'hax fid = {cfg.hax_fid}, hax endpoint = {cfg.hax_ep}, '
              f'HA fid = {cfg.ha_fid}')
-
-    _ha_message_bus_init(util)
 
     ffi = HaxFFI()
     herald = DeliveryHerald()