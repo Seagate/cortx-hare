6339d08263ecab166ec64069bfd9924e8de957eb
Mandar Sawant <mandar.sawant@seagate.com>
2022-02-02 09:14:53 -0700

    dtm: re-enable dynamic fids allocation
    Signed-off-by: Mandar Sawant <mandar.sawant@seagate.com>

--- hax/hax/motr/__init__.py
+++ hax/hax/motr/__init__.py
@@ -199,8 +199,8 @@ class Motr:
         try:
             util = self.consul_util
             # Disabling dynamic fids allocation until dtm is ready to consume.
-            # if util.is_proc_client(process_fid) and message.is_first_request:
-            #     util.alloc_next_process_fid(process_fid)
+            if util.is_proc_client(process_fid) and message.is_first_request:
+                util.alloc_next_process_fid(process_fid)
 
             # When stopping, there's a possibility that hax may receive
             # an entrypoint request from motr land. In order to unblock
@@ -315,11 +315,10 @@ class Motr:
                 continue
             # If its a client process then update the base fid to its full
             # fid.
-            # XXX Commenting until dtm is ready to consume dynamic fids.
-            # if (st.fid.container == ObjT.PROCESS.value and
-            #         self.consul_util.is_proc_client(st.fid)):
-            #     proc_full_fid = self.consul_util.get_process_full_fid(st.fid)
-            #     st.fid = proc_full_fid
+            if (st.fid.container == ObjT.PROCESS.value and
+                    self.consul_util.is_proc_client(st.fid)):
+                proc_full_fid = self.consul_util.get_process_full_fid(st.fid)
+                st.fid = proc_full_fid
             note = HaNoteStruct(st.fid.to_c(), st.status.to_ha_note_status())
             notes.append(note)
 
