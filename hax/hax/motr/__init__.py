f0db75aa37bd0431f0c80b9b748a7a690163b504
Mandar Sawant <mandar.sawant@seagate.com>
2022-02-15 13:26:10 -0700

    dtm: disable dynamic fids allocation
    Cortx-motr is not yet ready to consume dynamic fids.
    
    Solution:
    Disable dynamic fids allocation until Cortx-motr is ready to
    consume.
    
    Signed-off-by: Mandar Sawant <mandar.sawant@seagate.com>

--- hax/hax/motr/__init__.py
+++ hax/hax/motr/__init__.py
@@ -199,8 +199,8 @@ class Motr:
         try:
             util = self.consul_util
             # Disabling dynamic fids allocation until dtm is ready to consume.
-            if util.is_proc_client(process_fid) and message.is_first_request:
-                util.alloc_next_process_fid(process_fid)
+            # if util.is_proc_client(process_fid) and message.is_first_request:
+            #     util.alloc_next_process_fid(process_fid)
 
             # When stopping, there's a possibility that hax may receive
             # an entrypoint request from motr land. In order to unblock
@@ -320,6 +320,10 @@ class Motr:
                 proc_full_fid = self.consul_util.get_process_full_fid(st.fid)
                 st.fid = proc_full_fid
             note = HaNoteStruct(st.fid.to_c(), st.status.to_ha_note_status())
+            # if (st.fid.container == ObjT.PROCESS.value and
+            #         self.consul_util.is_proc_client(st.fid)):
+            #     proc_full_fid = self.consul_util.get_process_full_fid(st.fid)
+            #     st.fid = proc_full_fid
             notes.append(note)
 
             # For process failure, we report failure for the corresponding
