d1d4eda1ae0f1ff7b4fe2218cda999c6640a233e
Shreya Karmakar <shreya.karmakar@seagate.com>
2022-02-17 04:28:33 -0700

    CORTX-27237: Collect bytecount data from Motr and store in consul
    Currently, we have motr apis to fetch the bytecount from motr land.
    We need to use these apis and update the byte count of pool versions
    in Consul in regular intervals.
    
    Solution:
      Created a separate bytecount updater thread similar to fsStatsUpdater
      which updates bytecount information at regular intervals and stores
      it consul KV.
    
    Signed-off-by: Shreya Karmakar <shreya.karmakar@seagate.com>

--- hax/hax/bytecount.py
+++ hax/hax/bytecount.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,22 +16,21 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
-import datetime
 import logging
 from threading import Event
-
+from typing import List, Tuple
 from hax.exception import HAConsistencyException, InterruptedException
 from hax.motr import Motr, log_exception
-from hax.types import FsStatsWithTime, StoppableThread
+from hax.types import ByteCountStats, Fid, ObjHealth, StoppableThread
 from hax.util import ConsulUtil, wait_for_event
 
 LOG = logging.getLogger('hax')
 
 
-class FsStatsUpdater(StoppableThread):
+class ByteCountUpdater(StoppableThread):
     def __init__(self, motr: Motr, consul_util: ConsulUtil, interval_sec=5):
         super().__init__(target=self._execute,
-                         name='fs-stats-updater',
+                         name='byte-count-updater',
                          args=(motr, ))
         self.stopped = False
         self.consul = consul_util
@@ -46,30 +45,31 @@ class FsStatsUpdater(StoppableThread):
     @log_exception
     def _execute(self, motr: Motr):
         try:
-            LOG.info('filesystem stats updater thread has started')
+            LOG.info('byte-count updater thread has started')
             while not self.stopped:
                 if not self.consul.am_i_rc():
                     wait_for_event(self.event, self.interval_sec)
                     continue
-                if (not motr.is_spiel_ready() or (
-                        not all(self.consul.ensure_ioservices_running()))):
+                if not motr.is_spiel_ready():
                     wait_for_event(self.event, self.interval_sec)
                     continue
-                stats = motr.get_filesystem_stats()
-                if not stats:
-                    continue
-                LOG.debug('FS stats are as follows: %s', stats)
-                now_time = datetime.datetime.now()
-                data = FsStatsWithTime(stats=stats,
-                                       timestamp=now_time.timestamp(),
-                                       date=now_time.isoformat())
-                try:
-                    self.consul.update_fs_stats(data)
-                except HAConsistencyException:
-                    LOG.debug('Failed to update Consul KV '
-                              'due to an intermittent error. The '
-                              'error is swallowed since new attempts '
-                              'will be made timely')
+                processes: List[Tuple[Fid, ObjHealth]] = \
+                    self.consul.get_proc_fids_with_status(['ios'])
+                for ios, status in processes:
+                    if status == ObjHealth.OK:
+                        byte_count: ByteCountStats = motr.get_proc_bytecount(
+                            ios)
+                        LOG.debug('Received bytecount: %s', byte_count)
+                        if not byte_count:
+                            continue
+                        try:
+                            self.consul.update_pver_bc(byte_count)
+                        except HAConsistencyException:
+                            LOG.debug('Failed to update Consul KV '
+                                      'due to an intermittent error. The '
+                                      'error is swallowed since new attempts '
+                                      'will be made timely')
+
                 wait_for_event(self.event, self.interval_sec)
         except InterruptedException:
             # No op. _sleep() has interrupted before the timeout exceeded:
@@ -79,4 +79,4 @@ class FsStatsUpdater(StoppableThread):
         except Exception:
             LOG.exception('Aborting due to an error')
         finally:
-            LOG.debug('filesystem stats updater thread exited')
+            LOG.debug('byte-count updater thread exited')
