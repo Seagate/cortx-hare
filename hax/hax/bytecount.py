c3d49da863c7eab05680d9455941a557047b0c19
Shreya Karmakar <shreya.karmakar@seagate.com>
2022-02-18 05:08:30 -0700

    CORTX-27238: Categorise bytecount based on pool version state
    Based on pool version state for each pool version, it would require to
    calculate degraded, critical and healthy bytecounts.
    
    1. _get_pver_with_pver_status() in bytecount.py
       Getting a dictionary value of pver and it's state like below
            Ex: {'0x7600000000000001:0x8': PverState.M0_CPS_CRITICAL,
                 '0x7600000000000001:0x9': PverState.M0_CPS_HEALTHY
                }
    
    2. update_bc_for_dg_category in util.py
       which will update aggreagate bytecount value for each pool on consul.
       This function will update bytecount for subsequent dg state/category
       which must reflect on cluster status.
    
    Signed-off-by: Rahul Kumar <rahul.kumar@seagate.com>
    Signed-off-by: Shreya Karmakar <shreya.karmakar@seagate.com>

--- hax/hax/bytecount.py
+++ hax/hax/bytecount.py
@@ -18,10 +18,11 @@
 
 import logging
 from threading import Event
-from typing import List, Tuple
+from typing import Dict, List, Optional, Tuple
 from hax.exception import HAConsistencyException, InterruptedException
 from hax.motr import Motr, log_exception
-from hax.types import ByteCountStats, Fid, ObjHealth, StoppableThread
+from hax.types import (ByteCountStats, Fid, ObjHealth, PverState,
+                       StoppableThread)
 from hax.util import ConsulUtil, wait_for_event
 
 LOG = logging.getLogger('hax')
@@ -42,6 +43,28 @@ class ByteCountUpdater(StoppableThread):
         self.stopped = True
         self.event.set()
 
+    def _get_pver_with_pver_status(self,
+                                   motr: Motr) -> Optional[
+                                       Dict[str, PverState]]:
+        '''
+        Getting a dictionary value of pver and it's state like below example:
+        Ex: {'0x7600000000000001:0x8': PverState.M0_CPS_CRITICAL,
+             '0x7600000000000001:0x9': PverState.M0_CPS_HEALTHY
+            }
+        Pver data is stored in consul kv in format
+        key = ioservices/0x7200000000000001:0x20/pvers/
+              0x7600000000000001:0x6/users/1
+        value = {"bc": 4096, "object_cnt": 1}
+        '''
+        iosservice_items = self.consul.kv.kv_get('ioservices/', recurse=True)
+        pver_items = {}
+        if iosservice_items:
+            for k in iosservice_items:
+                p_ver = k['Key'].split('/')[3]
+                pver_items[p_ver] = motr.get_pver_status(Fid.parse(p_ver))
+            LOG.debug('Received pool version and status: %s', pver_items)
+        return pver_items
+
     @log_exception
     def _execute(self, motr: Motr):
         try:
@@ -55,6 +78,8 @@ class ByteCountUpdater(StoppableThread):
                     continue
                 processes: List[Tuple[Fid, ObjHealth]] = \
                     self.consul.get_proc_fids_with_status(['ios'])
+                if not processes:
+                    continue
                 for ios, status in processes:
                     if status == ObjHealth.OK:
                         byte_count: ByteCountStats = motr.get_proc_bytecount(
@@ -71,6 +96,11 @@ class ByteCountUpdater(StoppableThread):
                                       'will be made timely')
 
                 wait_for_event(self.event, self.interval_sec)
+
+                pver_items = self._get_pver_with_pver_status(motr)
+                if not pver_items:
+                    continue
+                self.consul.update_bc_for_dg_category(pver_items)
         except InterruptedException:
             # No op. _sleep() has interrupted before the timeout exceeded:
             # the application is shutting down.
