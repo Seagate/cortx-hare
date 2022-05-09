3e150cde9b70fd7193aaa81393f331d6201c12cc
root <root@localhost.localdomain>
2022-05-09 10:39:23 -0400

    Fixed syntax error
    Signed-off-by: hessio <patrick.hession@seagate.com>
    
    Removed unnecessary @staticmethod decorator
    
    Signed-off-by: hessio <patrick.hession@seagate.com>

--- hax/hax/consul/cache.py
+++ hax/hax/consul/cache.py
@@ -53,7 +53,6 @@ class InvocationCache:
         fun_dict[param_key] = ret_value
         self._calls[fn_name] = fun_dict
 
-    @staticmethod
     def _create_key_by_args(self, *args, **kwargs) -> str:
         return f'{args}{kwargs}'
 
