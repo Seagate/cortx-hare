820b7627a4b505f503d0f4b041c31670cdf5b5f2
SwapnilGaonkar7 <68695643+SwapnilGaonkar7@users.noreply.github.com>
2022-07-05 19:49:44 +0530

    CORTX-32421: [2.0.0-837-07603][v0.7.0][27N] Server pods going into CrashLoopBackOff state during deployment (#2133)
    Solution:
    Revert only caching related part of "CORTX-31920: get_machineid_by_nodename() may return None"
    
    This reverts commit fc55437fa66ad992e633b626dd50d51ec0e00c80
    
    Signed-off-by: Swapnil Gaonkar <swapnil.gaonkar@seagate.com>

--- hax/hax/util.py
+++ hax/hax/util.py
@@ -799,7 +799,7 @@ class ConsulUtil:
             node_val = self.kv.kv_get(node_key, kv_cache=kv_cache)
             data = node_val['Value']
             node_name: str = json.loads(data)['name']
-            if (self.get_node_health_status(node_name) !=
+            if (self.get_node_health_status(node_name, kv_cache=kv_cache) !=
                     'passing'):
                 obj_state = ObjHealth.OFFLINE.to_ha_note_status()
 
@@ -898,8 +898,10 @@ class ConsulUtil:
             raise HAConsistencyException(
                 'Failed to members data from Consul') from e
 
+    @uses_consul_cache
     def get_node_health_details(
-            self, node: str) -> Optional[List[Dict[str, Any]]]:
+            self, node: str,
+            kv_cache=None) -> Optional[List[Dict[str, Any]]]:
         """
         Returns the list of health checks (as it is reported by Consul, see
         'Health: node' section at
@@ -914,13 +916,14 @@ class ConsulUtil:
             raise HAConsistencyException(
                 f'Failed to get {node} node health: {e}')
 
-    def get_node_health_status(self, node: str) -> str:
+    @uses_consul_cache
+    def get_node_health_status(self, node: str, kv_cache=None) -> str:
         """
         Returns the node health status string returned by Consul.
         Possible return values: passing, warning, critical
         """
         try:
-            node_data = self.get_node_health_details(node)
+            node_data = self.get_node_health_details(node, kv_cache=kv_cache)
             # if not node_data or (not self.is_node_alive(node,
             #                                             kv_cache=kv_cache)):
             if not node_data:
@@ -974,6 +977,7 @@ class ConsulUtil:
             return name
         return None
 
+    @repeat_if_fails()
     @uses_consul_cache
     def get_node_name_by_machineid(self,
                                    machineid: str,
@@ -994,7 +998,7 @@ class ConsulUtil:
     def get_machineid_by_nodename(self,
                                   nodename: str,
                                   kv_cache=None,
-                                  allow_null=False) -> Optional[str]:
+                                  allow_null=False):
         """
         Returns the machine id by its node-name value or None if the given
         node-name doesn't correspond to any node.
@@ -1365,7 +1369,7 @@ class ConsulUtil:
             if state in (m0HaObjState.M0_NC_TRANSIENT.name,
                          m0HaObjState.M0_NC_FAILED.name):
                 node = self.get_ctrl_node(ctrl_fid, kv_cache=kv_cache)
-                if (self.get_node_health_status(node) ==
+                if (self.get_node_health_status(node, kv_cache=kv_cache) ==
                         'passing'):
                     return m0HaObjState.M0_NC_ONLINE
             else:
@@ -1390,7 +1394,7 @@ class ConsulUtil:
             if state in (m0HaObjState.M0_NC_TRANSIENT.name,
                          m0HaObjState.M0_NC_FAILED.name):
                 node = self.get_encl_node(encl_fid, kv_cache=kv_cache)
-                if (self.get_node_health_status(node) ==
+                if (self.get_node_health_status(node, kv_cache=kv_cache) ==
                         'passing'):
                     return m0HaObjState.M0_NC_ONLINE
             else:
@@ -1412,7 +1416,8 @@ class ConsulUtil:
             LOG.debug('Node=%s state=%s', node_fid, state)
             if state in (m0HaObjState.M0_NC_TRANSIENT.name,
                          m0HaObjState.M0_NC_FAILED.name):
-                if (self.get_node_health_status(node_name) ==
+                if (self.get_node_health_status(node_name,
+                                                kv_cache=kv_cache) ==
                         'passing'):
                     return m0HaObjState.M0_NC_ONLINE
             else:
@@ -1702,20 +1707,22 @@ class ConsulUtil:
                            kv_cache=None) -> MotrConsulProcInfo:
         proc_base_fid = self.get_process_base_fid(fid)
         key = f'processes/{proc_base_fid}'
-        status = self.kv.kv_get(key, allow_null=True)
+        status = self.kv.kv_get(key, kv_cache=kv_cache, allow_null=True)
         if status:
             val = json.loads(status['Value'])
             return MotrConsulProcInfo(val['state'], val['type'])
         else:
             return MotrConsulProcInfo('Unknown', 'Unknown')
 
+    @uses_consul_cache
     def get_process_status_local(self,
                                  fid: Fid,
-                                 proc_node=None) -> MotrConsulProcInfo:
+                                 proc_node=None,
+                                 kv_cache=None) -> MotrConsulProcInfo:
         proc_base_fid = self.get_process_base_fid(fid)
         this_node = self.get_local_nodename()
         key = f'{this_node}/processes/{proc_base_fid}'
-        status = self.kv.kv_get(key, allow_null=True)
+        status = self.kv.kv_get(key, kv_cache=kv_cache, allow_null=True)
         if status:
             val = json.loads(status['Value'])
             return MotrConsulProcInfo(val['state'], val['type'])
@@ -1829,10 +1836,6 @@ class ConsulUtil:
             cur_consul_status('passing', 'M0_CONF_HA_PROCESS_DTM_RECOVERED'):
             local_remote_health_ret(ObjHealth.OK,
                                     ObjHealth.OK),
-            # It is possible that Consul service status was reported as
-            # warning earlier and now the service is back online.
-            # Thus report the status as ONLINE for now. If the service
-            # goes offline Consul will report the same.
             cur_consul_status('passing', 'M0_CONF_HA_PROCESS_STOPPED'):
             local_remote_health_ret(ObjHealth.OK,
                                     ObjHealth.OFFLINE),
@@ -1858,7 +1861,8 @@ class ConsulUtil:
             local_remote_health_ret(ObjHealth.OFFLINE,
                                     ObjHealth.OFFLINE)}
         try:
-            node_data = self.get_node_health_details(node)
+            node_data = self.get_node_health_details(
+                node, kv_cache=kv_cache)
             if not node_data:
                 return ObjHealth.OFFLINE
             node_status = str(node_data[0]['Status'])
